099edf1fae2948441b746fe75b0c8b8d77539cfe
Ahmed Salah <ahmed.msalah99@gmail.com>
2021-03-19 16:31:19 +0200

    fix upload after adding buttons

--- loginapp/backend/public/javascripts/welcome/up.js
+++ loginapp/backend/public/javascripts/welcome/up.js
@@ -4,31 +4,51 @@ class up{
     {
 
     }
+ 
+
     upload_data(jwt){
-        var report1_butt = document.getElementById('report1');
-        var report2_butt = document.getElementById('report2');
-        var report3_butt = document.getElementById('report3');
-        if (report1_butt.checked){
-            let cols = ['DATE','COUNTRY','CITY','STORE','BRAND','PRODUCT','SOLDUNITS','ONHANDUNITS','SOLDAMOUNT','ONHANDAMOUNT']
-            let table = 'pos'
+        function get_cols_table(){
+            return new Promise((resolve, reject) => {
+            var report1_butt = document.getElementById('report1');
+            var report2_butt = document.getElementById('report2');
+            var report3_butt = document.getElementById('report3');
+            
+            if (report1_butt.checked===true){
+                let cols = ['DATE','COUNTRY','CITY','STORE','BRAND','PRODUCT','SOLDUNITS','ONHANDUNITS','SOLDAMOUNT','ONHANDAMOUNT']
+                let table = 'pos'
+                console.log('here');
+                resolve([cols,table])
+            }
+            else if (report2_butt.checked===true){
+                let cols = ['MONTH','COMPANY','COSTCENTER','REVENUES','OPERATINGCOSTS','OPERATINGEXPENSES','DEPRECIATIONSAMORTIZATIONS','INTERESTSTAXES','CURRENTASSETS','CURRENTLIABILITIES','TOTALASSETS','TOTALLIABILITIES']
+                let table = 'financial'
+                resolve([cols,table])
+            }
+            else if (report3_butt.checked===true){
+                let cols = ['DATE','STORE','ITEMFAMILY','ITEM','QUANTITYINSTOCK','UNITVALUE','TARGETSTOCKMIN','TARGETSTOCKMAX']
+                let table = 'inventory'
+                resolve([cols,table])
+            }
+        })
         }
-        else if (report2_butt.checked){
-            let cols = ['MONTH','COMPANY','COSTCENTER','REVENUES','OPERATINGCOSTS','OPERATINGEXPENSES','DEPRECIATIONSAMORTIZATIONS','INTERESTSTAXES','CURRENTASSETS','CURRENTLIABILITIES','TOTALASSETS','TOTALLIABILITIES']
-            let table = 'financial'
-        }
-        else if (report3_butt.checked){
-            let cols = ['DATE','STORE','ITEMFAMILY','ITEM','QUANTITYINSTOCK','UNITVALUE','TARGETSTOCKMIN','TARGETSTOCKMAX']
-            let table = 'inventory'
-        }
-
-
-
         var uploadButt = document.getElementById('upload');
+        
         const processor1 = new processor();
 
         uploadButt.onchange = function(e) {
+            
+           
+            
+        
+            
         let promise = processor1.processFile(e);
+        
         promise.then((json_df) => {
+            get_cols_table().then( (res)=>{
+            let cols = res[0]
+            let table = res[1]
+            console.log(cols)
+            console.log(table)
             console.log(json_df);
             const myHeaders = new Headers();
 
@@ -41,7 +61,9 @@ class up{
             body: JSON.stringify({'data':json_df,'cols':cols,'table':table}),
             }).then(response => response.json()).then(response =>console.log(response))
         })
+    });
 
         }
     }
+        
 }
